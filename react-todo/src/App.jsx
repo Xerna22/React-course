63b58d62bf56642c854a2aaddf14c7b9227fdaef
elias <eliascerna2000@gmail.com>
2024-04-12 20:28:30 -0600

    nuevos archivos del proyecto

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,5 +1,9 @@
-const App = () =>{
-  return(<h1 className="text-center md:text-right">Hola (～￣▽￣)～</h1> )
-}
+const App = () => {
+  return (
+    <h1 className=" px-4 sm:px-8 bg-sky-700   sm:py-3  hover:bg-sky-800 text-white">
+      Hola (～￣▽￣)～
+    </h1>
+  );
+};
 
-export default App
+export default App;
